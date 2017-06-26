42f02ca1039e651da37e7611de7100a1e2cbef80
Joshua Sones <joshuasones@dhcp-rhodes-544.eduroam.cornell.edu>
2017-06-26 12:02:37 -0400

    Corrected waypoints URL

--- requestHandler.py
+++ requestHandler.py
@@ -70,7 +70,7 @@ def parse_json(presets=False):
 
 
 	if (not presets):
-		url = urllib.urlopen("https://autonomous-bicyc-1486924645777.appspot.com/getwaypoints") #NEW
+		url = urllib.urlopen("https://abserver-168813.appspot.com/getwaypoints") #NEW
 		string = url.read()
 
 		#Make into dictionary
