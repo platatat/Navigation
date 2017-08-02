da1f2658ab01d8995db73512fbbf2dcfaf6d9554
Joshua Ross Even <joshua@Joshuas-MacBook-Pro.local>
2017-08-02 10:59:00 -0400

    style changes

--- kalman_node.py
+++ kalman_node.py
@@ -58,39 +58,38 @@ def listener():
         #rospy.loginfo(gps_matrix)
         #save gps state values for later plotting
         gps_data.append(gps_matrix)
-        if len(gps_data) >= 1:
-            C = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-            #If we have 
-            if len(gps_data) == 1:
-                P_initial = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-                x_pos = gps_matrix[:,0]
-                y_pos = gps_matrix[:,1]
-                yaw = gps_matrix[:,2]
-                v = gps_matrix[:,3]
-                v_0 = v.item(0)
-                yaw_0 = yaw.item(0)
-                x_dot_0 = v_0 * np.cos(yaw_0)
-                y_dot_0 = v_0 * np.sin(yaw_0)
-                s_initial = np.matrix([[x_pos.item(0)], [y_pos.item(0)], [x_dot_0], [y_dot_0]])
-                #output matrix - returns a tuple - first entry - kalman state (x,y,x',y')
-                #                             - second entry - prediction error (p)
-                output_matrix = kalman_real_time.kalman_no_loop(gps_matrix, C, s_initial, P_initial)
-            else:
-                output_matrix = kalman_real_time.kalman_no_loop(gps_matrix, C, 
-                                                     kalman_state_matrix, p_state_matrix) 
-            #rospy.loginfo(output_matrix.shape)
-            kalman_state_matrix = output_matrix[0] 
-            p_state_matrix = output_matrix[1]                                           
-            #Change output_matrix to a standard array for publishing
-            kalman_state = output_matrix[0].flatten().tolist()[0]
-            #rospy.loginfo(kalman_state)
-            p_state = output_matrix[1].flatten()
-            #save predicted state values for later plotting
-            #kalman_data.append(kalman_state_matrix) 
-            #pub.publish(layout, kalman_state)
-        pub.publish(layout, kalman_state)
-        rate.sleep()
-        #rospy.loginfo('SUCCESSFUL ITERATION')
+        C = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        #If we have 
+        if len(gps_data) == 1:
+            P_initial = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+            x_pos = gps_matrix[:,0]
+            y_pos = gps_matrix[:,1]
+            yaw = gps_matrix[:,2]
+            v = gps_matrix[:,3]
+            v_0 = v.item(0)
+            yaw_0 = yaw.item(0)
+            x_dot_0 = v_0 * np.cos(yaw_0)
+            y_dot_0 = v_0 * np.sin(yaw_0)
+            s_initial = np.matrix([[x_pos.item(0)], [y_pos.item(0)], [x_dot_0], [y_dot_0]])
+            #output matrix - returns a tuple - first entry - kalman state (x,y,x',y')
+            #                             - second entry - prediction error (p)
+            output_matrix = kalman_real_time.kalman_no_loop(gps_matrix, C, s_initial, P_initial)
+        else:
+            output_matrix = kalman_real_time.kalman_no_loop(gps_matrix, C, 
+                                                 kalman_state_matrix, p_state_matrix) 
+        #rospy.loginfo(output_matrix.shape)
+        kalman_state_matrix = output_matrix[0] 
+        p_state_matrix = output_matrix[1]                                           
+        #Change output_matrix to a standard array for publishing
+        kalman_state = output_matrix[0].flatten().tolist()[0]
+        #rospy.loginfo(kalman_state)
+        p_state = output_matrix[1].flatten()
+        #save predicted state values for later plotting
+        #kalman_data.append(kalman_state_matrix) 
+        #pub.publish(layout, kalman_state)
+    pub.publish(layout, kalman_state)
+    rate.sleep()
+    #rospy.loginfo('SUCCESSFUL ITERATION')
     #rospy.loginfo('Test was terminated')
     #rospy.spin()
     # Plot the GPS data
