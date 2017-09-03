5a26f1776f03e44a9117b779bd1c3f3e280b071d
Joshua Sones <joshuasones@dhcp-rhodes-649.redrover.cornell.edu>
2017-09-03 18:00:23 -0400

    Combined all the drivers into one file

--- kalman_driver.py
+++ kalman_driver.py
@@ -1,10 +1,18 @@
 """
-Script that is used to plot raw GPS data vs. kalman-filtered data that was created
-retroactively after a test. In order to use this script, you must
-download the correct gps csv (see testing instructions), and
-GPS_FILE below must be changed to the correct filepath.
+Used to plot raw GPS data vs. kalman-filtered data. In order to use this, you must
+download the correct csv files (see testing instructions), and change the relevant filepaths
+(starting at line 28).
 
-Position, speed, and yaw are all collected from GPS.
+At the bottom of the file, uncomment the function you want to use for plotting:
+
+Use gps_only_retro() to plot kalman filter data made retroactively using only the gps.
+You need the correct GPS_FILE for this.
+
+Use gps_imu_retro() to plot kalman filter data made retroactively using gps and imu data.
+You need the correct GPS_FILE and BIKE_STATE for this.
+
+Use real_time() to plot kalman filter data that was made in real time during a test.
+You need the correct GPS_FILE and KALMAN_FILE for this.
 """
 
 import csv
@@ -18,8 +26,13 @@ import kalman
 import requestHandler
 
 GPS_FILE = "/Users/joshuasones/Desktop/School/Bike Team/gps_2017-07-18~~02-36-30-PM-copy.csv"
+BIKE_STATE_FILE = "/Users/joshuasones/Desktop/bike_2017-07-18~~02-36-30-PM-copy.csv"
+KALMAN_FILE = "/Users/joshuasones/Desktop/School/Bike Team/kalman_pub_2017-08-02~~12-29-37-PM.csv"
 
-gps_data = []
+gps_psyt_data = []
+gps_pst_data = []
+bikestate_y_data = []
+kalman_data = []
 
 with open(GPS_FILE) as gps_file:
     gps_reader = csv.reader(gps_file, delimiter=",")
@@ -35,19 +48,85 @@ with open(GPS_FILE) as gps_file:
         # field8 is speed from the gps (meters per second)
         # field10 is timestep (miliseconds)
         x, y = requestHandler.math_convert(float(row[2]), float(row[3]))
-        gps_data.append([x, y, float(row[9])*np.pi/180, float(row[10]), float(row[12])])
+        gps_psyt_data.append([x, y, float(row[9])*np.pi/180, float(row[10]), float(row[12])])
+        gps_pst_data.append([x, y, float(row[10]), float(row[12])])
 
-# The Kalman filter wants the GPS data in matrix form
-gps_matrix = np.matrix(gps_data)
+with open(BIKE_STATE_FILE) as bike_state_file:
+    bike_state_reader = csv.reader(bike_state_file, delimiter=",")
+    header_row = True
+    for row in bike_state_reader:
+        if header_row:
+            
+            #Skip the first row, which contains the headers
+            header_row = False
+            continue
+        
+        #field9 is yaw in radians
+        bikestate_y_data.append([180+float(row[11])])
+        
+with open(KALMAN_FILE) as kalman_file:
+    kalman_reader = csv.reader(kalman_file, delimiter=",")
+    header_row = True
+    for row in kalman_reader:
+        if header_row:
 
-# Plot the GPS data
-plt.scatter(gps_matrix[:,0], gps_matrix[:,1], c='r', edgecolors="none")
+            # Skip the first row, which contains the headers
+            header_row = False
+            continue
 
-# Run the Kalman filter
-output_matrix = kalman.kalman_retro(gps_matrix)
+        # field5 is lat converted to x, field6 is long converted to y - cartesian
+        kalman_data.append([float(row[5]), float(row[6])])
 
-# Plot the Kalman output
-plt.scatter(output_matrix[:,0], output_matrix[:,1], edgecolors="none")
+def gps_only_retro(gps_data):
+  # The Kalman filter wants the GPS data in matrix form
+  gps_matrix = np.matrix(gps_data)
+  
+  # Plot the GPS data
+  plt.scatter(gps_matrix[:,0], gps_matrix[:,1], c='r', edgecolors="none")
+  
+  # Run the Kalman filter
+  output_matrix = kalman.kalman_retro(gps_matrix)
+  
+  # Plot the Kalman output
+  plt.scatter(output_matrix[:,0], output_matrix[:,1], edgecolors="none")
+  
+  # Show everything
+  plt.show()
 
-# Show everything
-plt.show()
+def gps_imu_retro(gps_data, imu_data):
+  
+  # The Kalman filter wants the data in combined matrix form
+  gps_matrix = np.matrix(gps_data)
+  bike_state_matrix = np.matrix(imu_data)
+  combined_matrix = np.insert(gps_matrix, 2, bike_state_matrix.flatten(), axis=1)
+  
+  # Plot the GPS data
+  plt.scatter(combined_matrix[:,0], combined_matrix[:,1], c='r', edgecolors="none")
+  
+  # Run the Kalman filter
+  output_matrix = kalman.kalman_retro(combined_matrix)
+  
+  # Plot the Kalman output
+  plt.scatter(output_matrix[:,0], output_matrix[:,1], edgecolors="none")
+  
+  # Show everything
+  plt.show()
+  
+def real_time(gps_data, kalman_data):
+  # The Kalman filter wants the GPS data in matrix form
+  gps_matrix = np.matrix(gps_data)
+  kalman_matrix = np.matrix(kalman_data)
+  
+  # Plot the GPS data
+  plt.scatter(gps_matrix[:,0], gps_matrix[:,1], c='r', edgecolors="none")
+  
+  # Plot the Kalman output
+  #plt.scatter(kalman_matrix[:,0], kalman_matrix[:,1], edgecolors="none")
+  
+  # Show everything
+  plt.show()
+  
+if __name__ == '__main__':
+  #gps_only_retro(gps_psyt_data)
+  gps_imu_retro(gps_pst_data, bikestate_y_data)
+  #real_time(gps_psyt_data, kalman_data)
